4285985b0dd70433dc45dba2be50e9412bd4a9e7
dev-John <jonatasoliveira1234@gmail.com>
2021-06-09 17:41:59 -0300

    Typescript essentials course

---
+++ objs-arrays-enums.ts
@@ -0,0 +1,33 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // this identifies a tuple type
+// } = {
+//   name: "Jonatas",
+//   age: 21,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [1, "Owner"],
+// };
+
+enum Role {
+  ADMIN, // defaults set it to 0
+  READ_ONLY = 3, // I can set a value if I want
+  AUTHOR = "AUTHOR", // the following item increases
+}
+
+const person = {
+  name: "Jonatas",
+  age: 21,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+let favouriteActivities: string[];
+favouriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
